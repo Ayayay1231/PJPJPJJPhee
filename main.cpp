630fb53c3f1e93dbaf4283e21c71cbd672cad7d3
Ayayay1231 <champ.25752@gmail.com>
2026-02-10 13:45:58 +0700

    new main new me wa

--- main.cpp
+++ main.cpp
@@ -2,17 +2,32 @@
 #include <SFML/Audio.hpp>
 #include<iostream>
 #include"Player.h"
+#include "Menu.h"
+#include"camera.h"
+#include"Playercombat.h"
+#include"Enemy.h"
+
+enum class State {Preparing,Player_turn,Enemy_turn,Check_win,Finished};
+
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1280, 1080), "test rabob eieieeeieiie");
+    sf::RenderWindow window(sf::VideoMode(800, 800), "test rabob eieieeeieiie");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Blue);
     shape.setPosition(sf::Vector2f(400.f,300.f));
+    PlayerC hero("Hero",100,45,40,3,"sprite/eps1.png");
+    hero.sprite.setPosition(200,400);
+    Enemy boss("HEE",150,20,5,0,"sprite/eps1.png");
+    boss.sprite.setPosition(600 ,400);
+
+    State curentState = State::Player_turn;
+
+    Camera camera;
 
     sf::Clock clock;
     Player luffy("sprite/animate.png");
-    
-
+    Menu menu(window.getSize().x,window.getSize().y);
+    luffy.setScale(5,5);
     sf::Font font;
     font.loadFromFile("CRA.ttf");
     if(!font.loadFromFile("CRA.ttf")){
@@ -22,6 +37,7 @@ int main() {
     sf::Text title("HAI HITLER!!!",font,150) ;
     title.setFillColor(sf::Color::Cyan);
     title.setPosition(300,50);
+
 
     sf:: RectangleShape s2({150.f,150.f});
     shape.setFillColor(sf::Color::Green);
@@ -55,10 +71,56 @@ int main() {
     while (window.isOpen()) {
         sf::Event event;
         float dt =clock.restart().asSeconds();
-        while (window.pollEvent(event)) {
+        while (window.pollEvent(event)) { //Game is working
             if (event.type == sf::Event::Closed)
                 window.close();
-            switch (event.type)
+
+            if(curentState == State::Player_turn && event.type == sf::Event::KeyPressed){
+                hero.isGuarding = false;
+                if(event.key.code == sf::Keyboard::Space){
+                    boss.takeDamage(hero.atk);
+                    std::cout << "hero attacks: " << hero.atk << std::endl;
+                    std::cout << "///////////////////////////////////////////" << std::endl;
+                    curentState = State::Check_win;
+                   // curentState = State::Enemy_turn;
+                }else if(event.key.code == sf::Keyboard::F ){
+                hero.isGuarding = true;
+                curentState = State::Enemy_turn;
+            }
+            }
+        
+
+    if(curentState == State::Check_win){
+        if(boss.hp<=0){ 
+            boss.hp = 0 ;
+            curentState = State::Finished;}
+        else curentState = State::Enemy_turn;}
+        
+    if(curentState == State::Enemy_turn){    
+
+        boss.takeAction(hero);
+        if(hero.hp <= 0 ){
+            hero.hp = 0;
+            curentState = State::Finished;
+
+        }else{
+            curentState = State::Player_turn;
+        }
+    }
+    if(curentState == State::Check_win){
+        if(boss.hp <= 0){
+            boss.hp = 0;
+           
+            curentState = State::Finished;
+
+
+        }else{
+            curentState = State::Enemy_turn;
+        }
+    }
+          //  window.setView(camera.GetView(window.getSize()));
+            
+           /* switch (event.type)
             {
             case sf::Event::KeyReleased:
                 switch(event.key.code){
@@ -69,25 +131,45 @@ int main() {
                     case sf::Keyboard::Down:
                     menu.MoveDown();
                     break;
+
+                   // case sf::Keyboard::Return:
+                 //   switch(menu.getPressItem())
                 }
                 break;
             
             default:
                 break;
-            }
+            }*/
 
        // luffy.upate(dt);
 
              
         }
-        luffy.upate(dt);
+       // luffy.upate(dt);
+
            
         
         window.clear();
-      // window.draw(title);
-      //  window.draw(nice);
-       // window.draw(move1);
-       luffy.draw(window);
+        if(curentState == State::Finished){
+            static bool print = false ;
+            if(!print){
+            if(hero.hp <= 0){
+                std::cout << "you lose" << std::endl;
+            }else if(boss.hp <= 0){
+                std::cout << "yeyeye" << std::endl;
+            }
+            print = true;
+        }
+        }
+       /* if(curentState == State::Player_turn){
+
+        }else if(curentState == State::Enemy_turn){
+            boss.takeAction(hero);
+            curentState = State::Player_turn;
+        }*/
+        hero.draw(window);
+        boss.draw(window);
+      
        
         
       //  window.draw(s2);
